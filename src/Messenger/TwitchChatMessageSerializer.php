96066cca09f3367e8b14f2a421bf68b176a1bed1
AaricDev <84964307+aaricdev@users.noreply.github.com>
2022-01-06 23:34:57 +0100

    Feature/chatbot (#6)
    * feat: Add some chatcommands to control from twitch chat

---
+++ src/Messenger/TwitchChatMessageSerializer.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Messenger;
+
+use App\Message\TwitchChatCommandMessage;
+use Symfony\Component\Messenger\Envelope;
+use Symfony\Component\Messenger\Transport\Serialization\SerializerInterface;
+use Symfony\Component\Serializer\SerializerInterface as Serializer;
+
+class TwitchChatMessageSerializer implements SerializerInterface
+{
+    protected Serializer $serializer;
+
+    public function __construct(Serializer $serializer)
+    {
+        $this->serializer = $serializer;
+    }
+
+    /**
+     * @throws \JsonException
+     */
+    public function decode(array $encodedEnvelope): Envelope
+    {
+        return new Envelope($this->serializer->deserialize($encodedEnvelope['body'], TwitchChatCommandMessage::class, 'json'));
+    }
+
+    public function encode(Envelope $envelope): array
+    {
+        return [
+            'body' => $this->serializer->serialize($envelope->getMessage(), 'json'),
+            'header' => null,
+        ];
+    }
+}
