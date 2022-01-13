957b26126bf3074086b5f5baf5cefb5eff33768d
Aaricdev <aaricdev@simply-stream.com>
2022-01-13 01:05:52 +0100

    feat: Add endpoint to create new tracker

---
+++ src/Form/SectionType.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Section;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\CollectionType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class SectionType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('title')
+            ->add('causes', CollectionType::class, [
+                'entry_type' => CauseType::class,
+                'allow_add' => true,
+                'prototype_name' => '__NAME__',
+                'property_path' => 'deaths'
+            ]);
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Section::class,
+        ]);
+    }
+}
